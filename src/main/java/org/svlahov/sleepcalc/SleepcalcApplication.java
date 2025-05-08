03951a2154a8f2b8c28ef0845a0f1e5868f5dd6a
svlahov <stefan.vlahov5@gmail.com>
2025-05-07 21:06:36 -0500

    Initial commit

---
+++ src/main/java/org/svlahov/sleepcalc/SleepcalcApplication.java
@@ -0,0 +1,13 @@
+package org.svlahov.sleepcalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleepcalcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SleepcalcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/svlahov/sleepcalc/SleepcalcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.svlahov.sleepcalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleepcalcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
