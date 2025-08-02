550218a6dcdf0c40022984aad181bd64980851db
Stefan Vlahov <stefan.vlahov5@gmail.com>
2025-08-02 18:19:50 -0500

    TDD: Add initial GetDebt test and implementation
    Testing the sleep controller GET endpoint for returning sleep debt with simple 0.0 value.

--- src/main/java/org/svlahov/sleepcalc/Controller/SleepController.java
+++ src/main/java/org/svlahov/sleepcalc/Controller/SleepController.java
@@ -1,54 +1,20 @@
 package org.svlahov.sleepcalc.Controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.math.RoundingMode;
 
 @RestController
-@RequestMapping("/api/sleep")
+@RequestMapping("api/sleep")
+@CrossOrigin(origins = "http://localhost:3000")
 public class SleepController {
 
-    private static final double IDEAL_SLEEP_HOURS = 7.5;
+    @GetMapping("/debt")
+    public double getCurrentSleepDebt() {
+        return 0.0;
+    }
 
-    @PostMapping
-    public ResponseEntity<Map<String, Object>> addSleepEntry(@RequestBody SleepEntry sleepEntry) {
-        double hoursSlept = sleepEntry.getHoursSlept();
-        double sleepDebtChange = calculateSleepDebtChange(hoursSlept);
-        
-        Map<String, Object> response = new HashMap<>();
-        response.put("sleepDebtChange", sleepDebtChange);
-        
-        String message;
-        if (sleepDebtChange > 0) {
-            message = "Sleep debt increased by " + sleepDebtChange + " hours";
-        } else {
-            message = "Sleep debt decreased by " + Math.abs(sleepDebtChange) + " hours";
-        }
-        
-        response.put("message", message);
-        
-        return ResponseEntity.ok(response);
-    }
-    
-    private double calculateSleepDebtChange(double hoursSlept) {
-        return IDEAL_SLEEP_HOURS - hoursSlept;
-    }
-    
-    // Inner class to represent the request body
-    public static class SleepEntry {
-        private double hoursSlept;
-        
-        public double getHoursSlept() {
-            return hoursSlept;
-        }
-        
-        public void setHoursSlept(double hoursSlept) {
-            this.hoursSlept = hoursSlept;
-        }
-    }
 }
---
+++ src/test/java/org/svlahov/sleepcalc/Controller/SleepControllerTest.java
@@ -0,0 +1,43 @@
+package org.svlahov.sleepcalc.Controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.closeTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(SleepController.class)
+class SleepControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @BeforeEach
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+    }
+
+    @Test
+    @DisplayName("GET /debt should return initial debt of 0.0")
+    void getInitialDebt() throws Exception {
+        mockMvc.perform(get("/api/sleep/debt"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", closeTo(0.0, 0.01)));
+    }
+}
