f8c40663ec8fbe20f3a60eae37db38e578c15be6
Stefan Vlahov <stefan.vlahov5@gmail.com>
2025-08-02 18:23:32 -0500

    Actual getCurrentSleepDebt impl
    Implementing getCurrentSleepDebt using BigDecimal for better accuracy

--- src/main/java/org/svlahov/sleepcalc/Controller/SleepController.java
+++ src/main/java/org/svlahov/sleepcalc/Controller/SleepController.java
@@ -1,20 +1,25 @@
 package org.svlahov.sleepcalc.Controller;
 
+import jdk.internal.net.http.HttpClientImpl;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.concurrent.atomic.AtomicReference;
 
 @RestController
 @RequestMapping("api/sleep")
 @CrossOrigin(origins = "http://localhost:3000")
 public class SleepController {
 
+    private AtomicReference<BigDecimal> currentSleepDebt = new AtomicReference<>(BigDecimal.ZERO);
+
     @GetMapping("/debt")
     public double getCurrentSleepDebt() {
-        return 0.0;
+        return currentSleepDebt.get().setScale(2, RoundingMode.HALF_UP).doubleValue();
     }
 
 }
