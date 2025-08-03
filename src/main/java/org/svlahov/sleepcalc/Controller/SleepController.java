a26232f1a67b2e77abca4a584c4f68e15d72a475
Stefan Vlahov <stefan.vlahov5@gmail.com>
2025-08-03 18:42:52 -0500

    Handle negative sleep amount input

--- src/main/java/org/svlahov/sleepcalc/Controller/SleepController.java
+++ src/main/java/org/svlahov/sleepcalc/Controller/SleepController.java
@@ -13,10 +13,19 @@ public class SleepController {
 
     private AtomicReference<BigDecimal> currentSleepDebt = new AtomicReference<>(BigDecimal.ZERO);
     private static final BigDecimal TARGET_SLEEP = new BigDecimal("7.5");
+    private static final BigDecimal ZERO = BigDecimal.ZERO;
 
     @GetMapping("/debt")
     public double getCurrentSleepDebt() {
         return currentSleepDebt.get().setScale(2, RoundingMode.HALF_UP).doubleValue();
+    }
+
+    @PostMapping
+    public double recordSleep(@RequestBody SleepInput input) {
+        BigDecimal hoursSlept = BigDecimal.valueOf(input.getHoursSlept());
+        if (hoursSlept.compareTo(ZERO) < 0) {
+            throw new IllegalArgumentException("Hours slept cannot be negative");
+        }
     }
 
     public static class SleepInput {
