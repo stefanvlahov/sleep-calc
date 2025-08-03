2ccd4b4e007a63f0d8c586f3360b900bd756d969
Stefan Vlahov <stefan.vlahov5@gmail.com>
2025-08-03 18:59:27 -0500

    Add implementation (tests not passing)
    Added the recordSleep POST method which includes sleep debt calculations.

--- src/main/java/org/svlahov/sleepcalc/Controller/SleepController.java
+++ src/main/java/org/svlahov/sleepcalc/Controller/SleepController.java
@@ -14,6 +14,10 @@ public class SleepController {
     private AtomicReference<BigDecimal> currentSleepDebt = new AtomicReference<>(BigDecimal.ZERO);
     private static final BigDecimal TARGET_SLEEP = new BigDecimal("7.5");
     private static final BigDecimal ZERO = BigDecimal.ZERO;
+    private static final BigDecimal ONE = BigDecimal.ONE;
+
+    private static final BigDecimal MAX_EFFECTIVE_DEBT = new BigDecimal("20.0");
+    private static final BigDecimal MIN_RECOVERY_FACTOR = new BigDecimal("0.3");
 
     @GetMapping("/debt")
     public double getCurrentSleepDebt() {
@@ -26,6 +30,36 @@ public class SleepController {
         if (hoursSlept.compareTo(ZERO) < 0) {
             throw new IllegalArgumentException("Hours slept cannot be negative");
         }
+
+        BigDecimal newDebt = currentSleepDebt.updateAndGet(previousDebt -> {
+            BigDecimal difference = hoursSlept.subtract(TARGET_SLEEP);
+            BigDecimal debtChange;
+
+            if (difference.compareTo(ZERO) > 0) {
+                BigDecimal recoveryFactor;
+                if (previousDebt.compareTo(ZERO) <= 0) {
+                    recoveryFactor = ONE;
+                } else {
+                    BigDecimal debtRatio = previousDebt.divide(MAX_EFFECTIVE_DEBT, 4, RoundingMode.HALF_UP);
+                    BigDecimal factorReduction = debtRatio.multiply(ONE.subtract(MIN_RECOVERY_FACTOR));
+                    BigDecimal calculatedFactor = ONE.subtract(factorReduction);
+                    recoveryFactor = calculatedFactor.max(MIN_RECOVERY_FACTOR);
+                }
+                debtChange = difference.multiply(recoveryFactor).negate();
+
+                System.out.println("Sleep > Target. Debt: " + previousDebt + ", Diff: " + difference + ", Factor: " + recoveryFactor + ", Change: " + debtChange);
+
+            } else {
+                debtChange = difference.negate();
+                System.out.println("Sleep <= Target. Debt: " + previousDebt + ", Diff: " + difference + ", Change: " + debtChange);
+            }
+
+            return previousDebt.add(debtChange);
+        });
+
+        System.out.println("New Debt: " + newDebt.setScale(2, RoundingMode.HALF_UP));
+
+        return newDebt.setScale(2, RoundingMode.HALF_UP).doubleValue();
     }
 
     public static class SleepInput {
