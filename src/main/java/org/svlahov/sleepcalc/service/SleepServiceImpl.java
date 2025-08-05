04470fa66194b1ba5ee6f7ade87b43834a583a3f
Stefan Vlahov <stefan.vlahov5@gmail.com>
2025-08-04 20:39:19 -0500

    Add SleepService tests
    As part of TDD add tests for SleepService implementation. Test are failing.

---
+++ src/main/java/org/svlahov/sleepcalc/service/SleepServiceImpl.java
@@ -0,0 +1,18 @@
+package org.svlahov.sleepcalc.service;
+
+public class SleepServiceImpl implements SleepService {
+    @Override
+    public double recordSleep(double hoursSlept) {
+        return 0;
+    }
+
+    @Override
+    public double getCurrentSleepDebt() {
+        return 0;
+    }
+
+    @Override
+    public void reset() {
+
+    }
+}
