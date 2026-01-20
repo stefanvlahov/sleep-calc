987b814b3300e33926fc663e09b62200c2248acf
Stefan Vlahov <stefan.vlahov5@gmail.com>
2026-01-19 20:20:33 -0600

    Import fix

--- src/test/java/org/svlahov/sleepcalc/service/ReportServiceTest.java
+++ src/test/java/org/svlahov/sleepcalc/service/ReportServiceTest.java
@@ -25,7 +25,6 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
